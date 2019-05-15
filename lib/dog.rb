4093eaa77dca2848d8b6885765eee52ef6082464
Joshua Suskin <jsuskin85@gmail.com>
2019-05-15 06:04:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
   def name
@@ -9,5 +10,13 @@ class Dog
   
   def name=(new_name)
     @name = new_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
   end
 end
